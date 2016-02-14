e29634d062b01b97b9d2860a87dd32654ee2d4a2
Shiv Verma <sverma11@illinois.edu>
2016-02-14 13:31:24 -0600

    added network logging and switched to csv

--- monitor.py
+++ monitor.py
@@ -3,9 +3,9 @@ from sys import argv, exit
 from time import gmtime, strftime
 from os import getpid, getppid
 import signal
+import csv
 
 def sigterm_handler(_signo, _stack_frame):
-    print("received sigterm")
     exit(0)
 
 signal.signal(signal.SIGTERM, sigterm_handler)
@@ -15,9 +15,18 @@ f = open('/tmp/monitor_pid.txt', 'w')
 f.write(str(pid) + "\n")
 f.close()
 
-while True:
-    line = ""
-    line += strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    line += " " + str(psutil.cpu_percent(interval=1, percpu=True))
-    line += " " + str(psutil.virtual_memory().percent) + "%"
-    print(line)
+with open(argv[1],'w') as csvfile:
+    network_fieldnames=['bytes_sent','bytes_recv','packets_sent','packets_recv']
+    fieldnames = ['current_time','cpu_percent_per_cpu','memory_percent'] + map(lambda x : 'net_io_'+x, network_fieldnames)
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    net_tot_before = psutil.net_io_counters()
+    while True:
+        line = {}
+        line['current_time'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        line['cpu_percent_per_cpu'] = str(psutil.cpu_percent(interval=1, percpu=True))
+        line['memory_percent'] = str(psutil.virtual_memory().percent)
+        net_tot_now = psutil.net_io_counters()
+        for idx, field in enumerate(network_fieldnames):
+            line['net_io_'+field] = str(net_tot_now[idx] - net_tot_before[idx])
+        writer.writerow(line)
--- monitor_cluster.sh
+++ monitor_cluster.sh
@@ -2,7 +2,7 @@
 
 while read line; do
     ssh -t $line "
-    nohup python ~/sverma11/Monitoring/monitor.py > ~/sverma11/Monitoring/LOGS/LOG_$(date -u +%F_%T).txt &
+    nohup python ~/sverma11/Monitoring/monitor.py ~/sverma11/Monitoring/LOGS/LOG_$(date -u +%F_%T).txt &
     sleep 0.1
     cat /tmp/monitor_pid.txt
     " < /dev/null
