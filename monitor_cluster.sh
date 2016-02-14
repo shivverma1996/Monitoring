66fb1da110dd550cf3b6cba6243c1f15b8682d16
Shiv Verma <sverma11@illinois.edu>
2016-02-14 21:26:00 +0000

    fix monitor_cluster.sh

--- monitor_cluster.sh
+++ monitor_cluster.sh
@@ -2,12 +2,12 @@
 
 while read line; do
     ssh -t $line "
-    nohup python ~/sverma11/Monitoring/monitor.py ~/sverma11/Monitoring/LOGS/LOG_$(date -u +%F_%T).txt &
+    nohup python ~/sverma11/Monitoring/monitor.py ~/sverma11/Monitoring/LOGS/LOG_$(date -u +%F_%T).csv &
     sleep 0.1
     cat /tmp/monitor_pid.txt
-    " < /dev/null
+    " < /dev/null &
 done < ~/machines
-$@
+sleep 1 && $@ && sleep 2
 while read line; do
     ssh -t $line "
     echo \"kill \$(cat /tmp/monitor_pid.txt)\"
